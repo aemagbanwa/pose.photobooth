4b2fc86bf15d45f171c06d5ce01539c3435bb8b4
Adrian Magbanwa <adrian.magbanwa@gmail.com>
2026-09-14 13:11:24 +0800

    Update inquiry endpoint

--- assets/js/inquiry-config.js
+++ assets/js/inquiry-config.js
@@ -1,3 +1,4 @@
 window.POSE_INQUIRY = {
-  endpoint: "https://script.google.com/macros/s/AKfycbyxrVHKs9CGLX4-WLpY1XZamNqF-ozfzrOy3MctJshjrlWWGI3tQUoX_ozjr3px7Yg4pQ/exec"
+  endpoint:
+    "https://script.google.com/macros/s/AKfycby5jTgjytc1Quh_Qp7236wYdqfUaQ2kaIXVa_xkWqBVfDEuCLoDgB9F9Kp5xd56z2t8/exec",
 };
